15fe53fe030911b3dd0ffbbee9f5dafc78ee44d4
Sam Malanchuk <malanchuksam@gmail.com>
2020-04-08 20:20:31 -0400

    implemented a djb2 hash method

--- src/hashtable.py
+++ src/hashtable.py
@@ -31,7 +31,10 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
+        hash = 5381
+        for x in key:
+            hash = (( hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
 
     def _hash_mod(self, key):
@@ -39,7 +42,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        return self._hash_djb2(key) % self.capacity
 
 
     def insert(self, key, value):
