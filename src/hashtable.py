d94412c0d891368ef36b15c8d326752d0e1454eb
Sam Malanchuk <malanchuksam@gmail.com>
2020-04-11 16:02:21 -0400

    modified resize for collision handling with single linked list

--- src/hashtable.py
+++ src/hashtable.py
@@ -161,20 +161,26 @@ class HashTable:
         # move all the elements from old to new
         for pair in old_storage:
             if pair is not None:
-                self.insert(pair.key, pair.value)
+                if pair.next is None:
+                    self.insert(pair.key, pair.value)
+                else:
+                    linkedPair = pair
+                    while linkedPair is not None:
+                        self.insert(linkedPair.key, linkedPair.value)
+                        linkedPair = linkedPair.next
         pass
 
 
-hashTable = HashTable(1)
-hashTable.insert('somekey1', 'somevalue1')
-hashTable.insert('somekey2', 'somevalue2')
-hashTable.insert('somekey3', 'somevalue3')
-hashTable.insert('somekey4', 'somevalue4')
+# hashTable = HashTable(1)
+# hashTable.insert('somekey1', 'somevalue1')
+# hashTable.insert('somekey2', 'somevalue2')
+# hashTable.insert('somekey3', 'somevalue3')
+# hashTable.insert('somekey4', 'somevalue4')
 
-hashTable.remove('somekey4')
-hashTable.remove('somekey1')
+# hashTable.remove('somekey4')
+# hashTable.remove('somekey1')
 
-hashTable.insert('somekey1', 'somevalue1')
+# hashTable.insert('somekey1', 'somevalue1')
 
 # print(hashTable.retrieve('somekey1'))
 # print(hashTable.retrieve('somekey2'))
@@ -190,30 +196,30 @@ hashTable.insert('somekey1', 'somevalue1')
 
 # hashTable.printStorage()
 
-# if __name__ == "__main__":
-#     ht = HashTable(2)
+if __name__ == "__main__":
+    ht = HashTable(2)
 
-#     ht.insert("line_1", "Tiny hash table")
-#     ht.insert("line_2", "Filled beyond capacity")
-#     ht.insert("line_3", "Linked list saves the day!")
+    ht.insert("line_1", "Tiny hash table")
+    ht.insert("line_2", "Filled beyond capacity")
+    ht.insert("line_3", "Linked list saves the day!")
 
-#     print("")
+    print("")
 
-#     # Test storing beyond capacity
-#     print(ht.retrieve("line_1"))
-#     print(ht.retrieve("line_2"))
-#     print(ht.retrieve("line_3"))
+    # Test storing beyond capacity
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
 
-#     # Test resizing
-#     old_capacity = len(ht.storage)
-#     ht.resize()
-#     new_capacity = len(ht.storage)
+    # Test resizing
+    old_capacity = len(ht.storage)
+    ht.resize()
+    new_capacity = len(ht.storage)
 
-#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-#     # Test if data intact after resizing
-#     print(ht.retrieve("line_1"))
-#     print(ht.retrieve("line_2"))
-#     print(ht.retrieve("line_3"))
+    # Test if data intact after resizing
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
 
-#     print("")
+    print("")
